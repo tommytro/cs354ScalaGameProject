2e7a770eef4a12e6966f41d322875ba1e3f8e22d
JoshFenske <60825313+joshfenske@users.noreply.github.com>
2022-10-19 11:09:44 -0600

    Updated build.gradle in desktop to support scala
    build.gradle in desktop can now compile scala. DesktopLauncher ~almost~ works now.

--- desktop/src/com/mygdx/game/DesktopLauncher.scala
+++ desktop/src/com/mygdx/game/DesktopLauncher.scala
@@ -1,18 +1,13 @@
-package com.mygdx.game;
+package com.mygdx.game
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.MyGdxGame
 
 object DesktopLauncher {
-  def main(arg: Array[String]) = {
+  def desktoplauncher(arg: Array[String]) = {
     val cfg = new Lwjgl3ApplicationConfiguration()
-    cfg.title = "puzzleplatform"
-    cfg.useGL20 = true
-    cfg.width = 800
-    cfg.height = 480
-    cfg.resizable = false
     
-    new Lwjgl3Application(new MyGdxGame(cfg.width, cfg.height), cfg)
+    new Lwjgl3Application(new MyGdxGame(), cfg)
   }
 }
