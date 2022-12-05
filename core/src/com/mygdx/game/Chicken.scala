9cb03759c68ddadfa4e39c53bb022b6c5002d344
tommytro <85188391+tommytro@users.noreply.github.com>
2022-12-05 11:06:48 -0700

    changed chicken naming

--- core/src/com/mygdx/game/Chicken.scala
+++ core/src/com/mygdx/game/Chicken.scala
@@ -16,13 +16,13 @@ import scala.util.Random
 class Chicken(x:Float, y:Float){
     
     var moveSpeed : Float = 4
-    var farmer: Rectangle = new Rectangle(x, y, 0, 0)
-	val farmerTex: Texture = GameScreen.chickTex
+    var chicken: Rectangle = new Rectangle(x, y, 0, 0)
+	val chickenTex: Texture = GameScreen.chickTex
 	val idleLeftTex = new Texture("chick_R.png")
 	val idleRightTex = new Texture("chick_L.png")
 	var idleRightFrames: Array[Array[TextureRegion]] = TextureRegion.split(idleRightTex, idleRightTex.getWidth() / 2, idleRightTex.getHeight() / 1)
 	var idleLeftFrames: Array[Array[TextureRegion]] = TextureRegion.split(idleLeftTex, idleLeftTex.getWidth() / 2, idleLeftTex.getHeight() / 1)
-	var walkFrames: Array[Array[TextureRegion]] = TextureRegion.split(farmerTex, farmerTex.getWidth() / 4, farmerTex.getHeight() / 2)
+	var walkFrames: Array[Array[TextureRegion]] = TextureRegion.split(chickenTex, chickenTex.getWidth() / 4, chickenTex.getHeight() / 2)
 	var idleAnimationLeft = new Animation(.162f, idleLeftFrames(0): _*)
 	var idleAnimationRight = new Animation(.162f, idleRightFrames(0): _*)
 	var moveLeftAnimation = new Animation(.162f, walkFrames(0): _*)
@@ -45,12 +45,12 @@ class Chicken(x:Float, y:Float){
 			}else if(facingRight == true){
 			currAnimation = moveRightAnimation
 			}
-			farmer.y = farmer.y + 175 * Gdx.graphics.getDeltaTime()
+			chicken.y = chicken.y + 175 * Gdx.graphics.getDeltaTime()
 		}
 		else if(rand > 4500 && rand < 5000){
 			facingRight = false
 			currAnimation = moveLeftAnimation
-            farmer.x = farmer.x - 175 * Gdx.graphics.getDeltaTime();
+            chicken.x = chicken.x - 175 * Gdx.graphics.getDeltaTime();
         }
 		else if(rand > 6500 && rand < 7000){
 
@@ -60,12 +60,12 @@ class Chicken(x:Float, y:Float){
 			currAnimation = moveRightAnimation
 			}
 
-			farmer.y = farmer.y - 175 * Gdx.graphics.getDeltaTime();
+			chicken.y = chicken.y - 175 * Gdx.graphics.getDeltaTime();
 		}
 		else if(rand > 9500){
 			facingRight = true
 			currAnimation = moveRightAnimation
-			farmer.x = farmer.x + 175 * Gdx.graphics.getDeltaTime();
+			chicken.x = chicken.x + 175 * Gdx.graphics.getDeltaTime();
 		}
 		else{
 			if(facingRight == false){
@@ -77,7 +77,7 @@ class Chicken(x:Float, y:Float){
 		stateTime = stateTime + Gdx.graphics.getDeltaTime()
 		currentFrame = currAnimation.getKeyFrame(stateTime, true)
 		spriteBatch.begin()
-		spriteBatch.draw(currentFrame, farmer.x, farmer.y, 32, 32)
+		spriteBatch.draw(currentFrame, chicken.x, chicken.y, 32, 32)
 		spriteBatch.end()
 	}
 }
